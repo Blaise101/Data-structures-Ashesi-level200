21968e614398ae890d2d1eaf9c2ab536d05b28e2
Blaise202 <izerimanablaise5@gmail.com>
2026-05-20 14:18:10 +0000

    DotEqual function

---
+++ dotequal.java
@@ -0,0 +1,8 @@
+class DotEqual {
+    public static void main(String[] args) {
+        Integer number1 = 5;
+        Integer number2 = 5;
+        boolean isEqual = number1.equals(number2) ;
+        System.out.println("Are the numbers equal? " + isEqual);
+    }
+}
