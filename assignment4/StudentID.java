8006cc16f9b77571fb2047534e0fe3017b26e0e4
Blaise Izerimana <izerimanablaise5@gmail.com>
2026-08-06 23:08:17 +0000

    Create StudentID.java

---
+++ assignment4/StudentID.java
@@ -0,0 +1,29 @@
+public class StudentID{
+    private final String id;
+
+    public StudentID(String id){
+        this.id = id;
+    }
+
+    public string getID(){
+        return id;
+    }
+
+    @Override
+    public String toString(){
+        return id; 
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+        StudentID other = (StudentID) obj;
+        return id != null ? id.equals(other.id) : other.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
